2cd06db4736009c51b39b41c6fa1243cf5ac3607
Patrick McDowell <kuhlthing@gmail.com>
2020-01-17 23:29:59 -0500

    added html generator

--- index.js
+++ index.js
@@ -7,3 +7,64 @@ var gs = require('github-scraper');
 const writeFileAsync = util.promisify(fs.writeFile);
 const gsPromise = util.promisify(gs);
 var github;
+
+
+
+function generateHTML(answers, userData, gsData) {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+          <title>Document</title>
+          <style>
+          .jumbotron {
+              color: ${answers.color};
+              background: mintcream;
+              text-align: center;
+          }
+          img{
+              border-radius: 50%;
+          }
+          h1, p, h2, h3{
+            color: ${answers.color};
+          }
+          .card {
+            color: ${answers.color};
+            background: ${answers.background};
+          }
+      </style>
+      </head>
+      <body>
+          
+          <div class="jumbotron">
+          <img src="${userData.githubPic}" class="rounded-circle mx-auto d-block mb-5" alt="${userData.githubName}s's picture">
+              <h1 class="display-4">${userData.githubName}</h1>
+              <p class="lead">I'm from ${userData.githubLocation}.</p>
+              <h3 class="lead">${userData.githubBio}</h3>
+              <div class="card-deck">
+              <div class="card"><h4>Number of github repos:</h4> <p>${userData.githubRepos}</p></div>
+              <div class="card"><h4>Number of github followers:</h4> <p>${userData.githubFollowers}</p></div>
+              <div class="card"><h4>Number of github following:</h4> <p>${gsData.following}</p></div>
+              <div class="card"><h4>Number of github stars:</h4> <p>${gsData.stars}</p></div>
+              </div>
+  
+              <hr class="my-4">
+              <p>Here are the ways you can reach me.</p>
+              <a class="btn btn-primary btn-lg" href="https://www.google.com/maps/place/${userData.githubLocation}/" role="button" target="blank">Location</a>
+              <a class="btn btn-primary btn-lg" href="${userData.githubURL}" role="button" target="blank">github</a>
+            </div>
+      
+      
+      
+      
+      
+          <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      </body>
+      </html>`;
+  }
